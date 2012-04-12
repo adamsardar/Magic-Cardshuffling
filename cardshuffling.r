7295b0df66dbaa23084d5042ffdce7db9aca743c
Adam Sardar <adam@sardar.me.uk>
2012-04-12 16:34:28 +0100

    Added several more functions to cardshuffling.r for testing bias of a shuffle

--- cardshuffling.r
+++ cardshuffling.r
@@ -18,6 +18,7 @@ Bayer.Diaconis.split<-function(decksize,prob=0.5){
 
 
 split.deck<-function(deck,prob=0.5){
+  #prob is important in deciding where in a deck the cut comes on average
   
   deck.size<-length(deck)
   cut.point<-Bayer.Diaconis.split(deck.size,prob)
@@ -153,6 +154,7 @@ riffle.shuffle <- function(deck)
 
 cut.deck <- function(deck,prob=0.5)
 {
+  #Cut the deck and place one pile atop the other
   deck.size<-length(deck)
   cuts<-split.deck(deck,prob) #Bayer Diaconis split
      
@@ -166,6 +168,7 @@ cut.deck <- function(deck,prob=0.5)
 
 fischer.yates.shuffle <- function(deck)
 {
+  # See http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle for more details
   deck.size<-length(deck)
   shuffled.deck<-deck
   
@@ -212,7 +215,7 @@ pile.shuffle <- function(deck,Npiles=7)
 }
 
 beta.model.split.deck<-function(deck,alpha=2,beta=7)
-#The values for alpha and beta were chosen as they fit a nice dstirbution as a prior for deck proportion
+#The values for alpha and beta were chosen as they fit a nice dstribution as a prior for deck proportion
 {
   
   split.prob<-rbeta(1,alpha,beta)
@@ -283,4 +286,127 @@ beta.consecutive.overhand.shuffle <- function(deck)
   shuffled.deck<-c(cut.two,shuffled.deck)
   
   return(shuffled.deck)
+}
+
+six.piles.d6.assigned <- function(deck,Npiles=6)
+{
+  deck.size<-length(deck)
+  piles<-vector("list",Npiles)
+  
+  for(i in 1:deck.size){
+    
+    index<-sample(1:Npiles,1,replace=TRUE)
+    #Roll a dN to see which pile we assign a card to
+    
+    current.pile<-piles[[index]]
+    piles[[index]]<-c(deck[i], piles[[index]])
+    #Add card to the top of a pile
+  }
+  
+  shuffled.deck<-c()
+  
+  for(pile in 1:Npiles){
+    
+    shuffled.deck<-c(piles[[pile]],shuffled.deck)
+  }
+  #Concatenate the piles together, linearly
+  
+  return(shuffled.deck)
+}
+
+six.piles.d6.assigned.random.concatenate <- function(deck,Npiles=6)
+{
+  deck.size<-length(deck)
+  piles<-vector("list",Npiles)
+  
+  for(i in 1:deck.size){
+    
+    index<-sample(1:Npiles,1,replace=TRUE)
+    #Roll a dN to see which pile we assign a card to
+    
+    current.pile<-piles[[index]]
+    piles[[index]]<-c(deck[i], piles[[index]])
+    #Add card to the top of a pile
+  }
+  
+  shuffled.deck<-c()
+  
+  concatenate.order<-sample(1:Npiles,Npiles,replace=FALSE)
+  
+  for(pile in concatenate.order){
+    
+    shuffled.deck<-c(piles[[pile]],shuffled.deck)
+  }
+  #Concatenate the piles together at uniform random
+  
+  return(shuffled.deck)
+}
+
+count.down.to.random.card <- function(deck,card)
+#Function to cut the deck of cards and then count down until we find the card of interest. Returned is the number of cards until that point
+{
+
+	deck.size<-length(deck)
+  	
+  	#Uniform split the deck and then place one ontop of the other
+	cut.point = sample(0:deck.size,1)
+
+    if(cut.point == 0){
+    
+    deck.cut.two.indicies<-seq(1,deck.size)
+    
+    cut.one<-c()
+    cut.two<-deck[deck.cut.two.indicies]
+    
+  }else if(cut.point == deck.size){
+    
+    deck.cut.one.indicies<-seq(1,deck.size)
+    
+    cut.one<-deck[deck.cut.one.indicies]
+    cut.two<-c()
+        
+  }else{
+    
+    deck.cut.one.indicies<-seq(1,cut.point)
+    deck.cut.two.indicies<-seq(cut.point+1,deck.size)
+    
+    cut.one<-deck[deck.cut.one.indicies]
+    cut.two<-deck[deck.cut.two.indicies]
+  }
+
+  deck<-c(cut.one,cut.two)
+  
+  index<-1
+    
+  while(deck[index] != card){
+  
+  	index<-index+1
+  	
+  	if(index > deck.size){stop("Card not found in deck - a serious error has occured!")}
+  }
+  #Count the number of cards up until the card of choice
+  
+  return(index)
+}
+
+onehundred.shuffles.search <- function(deck,card,iterations=100,nshuffles=1){
+
+	deck.size<-length(deck)
+ 	results<-rep(1,iterations)
+ 	#Preallocate
+
+	for(i in 1:iterations){
+		
+		newdeck<-deck
+		
+		for(j in 1:nshuffles){
+			
+			newdeck<-beta.consecutive.overhand.shuffle(newdeck)
+		}
+						
+		count<-count.down.to.random.card(newdeck,card)
+		results[i]<-count
+	}
+	
+	return(results)
 }
